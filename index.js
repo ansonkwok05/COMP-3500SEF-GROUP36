51aebed249fc96053aaae018fdb903f03068a469
ansonkwok05 <kwokanson05@gmail.com>
2025-11-09 18:13:45 +0800

    Bare bone express server.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const EXPRESS = require("express");
+const APP = EXPRESS();
+const PORT = process.env.PORT || 8080;
+
+const login = require("src/login.js");
+
+APP.use(EXPRESS.static("public"));
+APP.use("/", EXPRESS.static("public/index.html"));
+
+APP.get("/", (req, res) => {
+    res.send("public/index.html");
+});
+
+APP.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+});
---
+++ src/login.js
@@ -0,0 +1,3 @@
+// user data implemented with an object
+// todo: change this to a more acceptable way (using a database)
+let user_data = {};
