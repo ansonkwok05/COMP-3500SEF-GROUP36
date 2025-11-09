b515e9923f41d4dec36197b5e7bfde88f37f7f7d
ansonkwok05 <kwokanson05@gmail.com>
2025-11-09 18:36:31 +0800

    Basic html serving.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const EXPRESS = require("express");
 const APP = EXPRESS();
+
+const LOGIN = require("./src/login.js");
+
 const PORT = process.env.PORT || 8080;
+const ERR_PAGE = "404";
 
-const login = require("src/login.js");
+APP.use("/index.html", (req, res) => res.send(ERR_PAGE));
+APP.use("/", EXPRESS.static("./public/index.html"));
+APP.use(EXPRESS.static("./public"));
 
-APP.use(EXPRESS.static("public"));
-APP.use("/", EXPRESS.static("public/index.html"));
-
-APP.get("/", (req, res) => {
-    res.send("public/index.html");
+APP.use((req, res) => {
+    res.send(ERR_PAGE);
 });
 
 APP.listen(PORT, () => {
--- src/login.js
+++ src/login.js
@@ -1,3 +1,8 @@
 // user data implemented with an object
 // todo: change this to a more acceptable way (using a database)
 let user_data = {};
+function checkCredentials(email, password) {
+    // XD
+}
+
+module.exports = {checkCredentials};
