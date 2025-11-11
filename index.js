353bcc3488e9f869137dd7bfcb9f4e972fa99a5a
ansonkwok05 <kwokanson05@gmail.com>
2025-11-12 07:00:40 +0800

    Separate routes for authentication, limiting unauthorized access to only login, register and index page. Use express session to store userID in client browser as cookie to allow clients to stay logged in if they logged in and left the page.

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const EXPRESS = require("express");
+const SESSION = require("express-session");
 const BODYPARSER = require("body-parser");
 const APP = EXPRESS();
 
+const UTILS = require("./src/utils.js");
 const USER_MANAGER = require("./src/user_manager.js");
 const RESTAURANT = require("./src/restaurant.js");
 
 const PORT = process.env.PORT || 8080;
-const ERR_MESSAGE = "Page not found";
+const ERR_MESSAGE = "404 Page not found";
 
-// expose ./public folder
-// "/index.html" is replaced with "/"
-APP.use("/index.html", (req, res) => res.send(ERR_PAGE));
-APP.use("/", EXPRESS.static("./public/index.html"));
-APP.use(EXPRESS.static("./public"));
+// expose ./auth folder, which both authorized and unauthorized people can access
+APP.use(EXPRESS.static("./auth"));
 
 APP.use(BODYPARSER.urlencoded({extended: true}));
+
+// session for authorization after user log in
+APP.use(
+    SESSION({
+        secret: process.env.SESSION_SECRET || UTILS.generate_uuid(16),
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            path: "/",
+            httpOnly: false,
+            maxAge: 24 * 60 * 60 * 1000,
+        },
+    })
+);
 
 APP.post("/register", (req, res) => {
     const {email, password, name, address} = req.body;
@@ -37,12 +50,28 @@ APP.post("/login", (req, res) => {
 
     if (USER_MANAGER.login_user(email, password)) {
         console.log(`Login Success: ${email}, ${password}`);
+
+        // set session userID so user only need to sign in once per session
+        req.session.userID = USER_MANAGER.get_user_id(email);
         res.status(200).send("Logged in");
     } else {
         console.log(`Login Fail: ${email}, ${password}`);
         res.status(401).send("Log in failed");
     }
 });
+
+// check authentication status, block unauthenticated users
+APP.use((req, res, next) => {
+    if (req.session && req.session.userID) {
+        // userID exists, but still need to validate
+        if (USER_MANAGER.validate_user_id(req.session.userID)) return next();
+    }
+    console.log(`Redirected connection to /login.html`);
+    res.redirect("/login.html");
+});
+
+// expose ./public to authorized users only
+APP.use(EXPRESS.static("public"));
 
 APP.get("/api/restaurants", (req, res) => {
     // return list of restaurant
