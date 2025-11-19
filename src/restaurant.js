80ad6a51fc8fda5d6f8f02696501669a4ec5b163
ansonkwok05 <kwokanson05@gmail.com>
2025-11-20 07:17:49 +0800

    Remove deprecated codes (json as database, now using sqlite)

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,71 +1,3 @@
-// hardcoded restaurant data implemented with a js object
-// this data is created for testing only
-// todo: change this to a more acceptable way (using a database)
-let restaurant_data = {
-    1: {
-        name: "McDonald's",
-        address: "12 Street",
-        cuisine: "Fast-food",
-        rating: 3.4,
-        menu_items: {
-            1: {
-                name: "Big Mac",
-                price: 46,
-                description: "Burger with patty",
-            },
-            2: {
-                name: "McChicken",
-                price: 35,
-                description: "Burger with crispy chicken",
-            },
-        },
-    },
-    2: {
-        name: "KFC",
-        address: "345 Street",
-        cuisine: "Fast-food",
-        rating: 3.5,
-        menu_items: {
-            1: {
-                name: "Chicken Bucket",
-                price: 138,
-                description: "6 pieces of crispy chicken",
-            },
-            2: {
-                name: "Chicken Tenders",
-                price: 82,
-                description: "Chicken strips coated in seasoning",
-            },
-        },
-    },
-    3: {
-        name: "Pizza Hut",
-        address: "67 Street",
-        cuisine: "Italian",
-        rating: 3.6,
-        menu_items: {
-            1: {
-                name: "Pepperoni Supreme Pizza",
-                price: 199,
-                description: "Big pepperoni pizza",
-            },
-        },
-    },
-    4: {
-        name: "Saizeria",
-        address: "89 Street",
-        cuisine: "Italian",
-        rating: 3.7,
-        menu_items: {
-            1: {
-                name: "Pineapple & Bacon Pizza",
-                price: 58,
-                description: "Pizza with pineapple and bacon toppings",
-            },
-        },
-    },
-};
-
 const sqlite3 = require("sqlite3");
 
 // sqlite db connection
@@ -75,8 +7,6 @@ let db;
 function initialize_db(db_path) {
     db = new sqlite3.Database(db_path);
 }
-
-// todo convert to using sqlite
 
 async function get_restaurant_list() {
     return new Promise((resolve) => {
@@ -122,4 +52,4 @@ function update_menu_item(restaurant_id, menu_item_id, new_menu_item) {
     return true;
 }
 
-module.exports = {initialize_db, get_restaurant_list, get_menu};
+module.exports = { initialize_db, get_restaurant_list, get_menu };
